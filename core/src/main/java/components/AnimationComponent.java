643429316eb9d29a7ba70255e6991b750e2beb27
joachimhviid <joac1099@gmail.com>
2024-03-26 18:55:45 +0100

    Test basic setup with animations

---
+++ core/src/main/java/components/AnimationComponent.java
@@ -0,0 +1,60 @@
+package components;
+
+import com.almasb.fxgl.core.math.FXGLMath;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.texture.AnimatedTexture;
+import com.almasb.fxgl.texture.AnimationChannel;
+import javafx.geometry.Point2D;
+import javafx.util.Duration;
+
+public class AnimationComponent extends Component {
+    private int speed = 0;
+
+    private AnimatedTexture texture;
+    private AnimationChannel animIdle, animRun;
+
+    public AnimationComponent() {
+        animIdle = new AnimationChannel(FXGL.image("player_ready.png"), 6, 50, 48, Duration.seconds(1), 0, 5);
+        animRun = new AnimationChannel(FXGL.image("player_run.png"), 6, 50, 489, Duration.seconds(1), 0, 5);
+
+        texture = new AnimatedTexture(animIdle);
+    }
+
+    @Override
+    public void onAdded() {
+        entity.getTransformComponent().setScaleOrigin(new Point2D(16, 21));
+        entity.getViewComponent().addChild(texture);
+    }
+
+    @Override
+    public void onUpdate(double tpf) {
+        entity.translateX(speed * tpf);
+
+        if (speed != 0) {
+
+            if (texture.getAnimationChannel() == animIdle) {
+                texture.loopAnimationChannel(animRun);
+            }
+
+            speed = (int) (speed * 0.9);
+
+            if (FXGLMath.abs(speed) < 1) {
+                speed = 0;
+                texture.loopAnimationChannel(animIdle);
+            }
+        }
+    }
+
+    public void moveRight() {
+        speed = 150;
+
+        getEntity().setScaleX(1);
+    }
+
+    public void moveLeft() {
+        speed = -150;
+
+        getEntity().setScaleX(-1);
+    }
+}
--- core/src/main/java/launcher/GameLauncher.java
+++ core/src/main/java/launcher/GameLauncher.java
@@ -2,18 +2,79 @@ package launcher;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.GameScene;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import components.AnimationComponent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.text.Text;
 
 public class GameLauncher extends GameApplication {
-    public static void main(String[] args) {
-      System.out.println("Hello World!");
-      launch(args);
-    }
+  private Entity player;
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    launch(args);
+  }
 
   @Override
   protected void initSettings(GameSettings gameSettings) {
-    gameSettings.setWidth(800);
     gameSettings.setHeight(600);
+    gameSettings.setWidthFromRatio(16 / 9.0);
     gameSettings.setTitle("Boat to England");
     gameSettings.setVersion("0.1");
+  }
+
+  @Override
+  protected void initInput() {
+    Input input = FXGL.getInput();
+    input.addAction(new UserAction("Move Left") {
+      protected void onAction() {
+        System.out.println("Move left");
+        player.getComponent(AnimationComponent.class).moveLeft();
+      }
+    }, KeyCode.A);
+    input.addAction(new UserAction("Move Right") {
+      protected void onAction() {
+        System.out.println("Move right");
+        player.getComponent(AnimationComponent.class).moveRight();
+      }
+    }, KeyCode.D);
+    input.addAction(new UserAction("Move Up") {
+      protected void onAction() {
+        System.out.println("Move up");
+      }
+    }, KeyCode.W);
+    input.addAction(new UserAction("Move Down") {
+      protected void onAction() {
+        System.out.println("Move down");
+      }
+    }, KeyCode.S);
+  }
+
+  @Override
+  protected void initGame() {
+    System.out.println("Game initialized");
+    GameScene scene = FXGL.getGameScene();
+    scene.setBackgroundColor(javafx.scene.paint.Color.LIGHTBLUE);
+    player = FXGL.entityBuilder()
+      .at(400, 300)
+      .with(new AnimationComponent())
+      .buildAndAttach();
+  }
+
+  @Override
+  protected void initPhysics() {
+    System.out.println("Physics initialized");
+  }
+
+  @Override
+  protected void initUI() {
+    System.out.println("UI initialized");
+      Text text = new Text("Hello World!");
+
+      FXGL.addUINode(text, 100, 100);
   }
 }

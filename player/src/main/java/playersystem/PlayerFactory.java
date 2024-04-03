d1c8cd176b5068175397635ed365d054d3a83333
MacBech <115424555+MacBech@users.noreply.github.com>
2024-04-03 18:42:40 +0200

    tweak

--- player/src/main/java/playersystem/PlayerFactory.java
+++ player/src/main/java/playersystem/PlayerFactory.java
@@ -2,14 +2,16 @@ package playersystem;
 
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
 
-public class PlayerFactory {
+//@SetEntityFactory
+public class PlayerFactory implements EntityFactory {
 
     // Is this right?
     @Spawns("player")
-//    public Entity newPlayer(SpawnData data) {
+//  public Entity newPlayer(SpawnData data) {
     public Entity newPlayer() {
         return FXGL.entityBuilder()
                 .with(new AnimationComponent())

67bf7b4adc4366d4cf62bce571b22e7eb646a88b
hemgaard <pehem22@student.sdu.dk>
2024-04-21 13:12:02 +0200

    WeaponSPI created

---
+++ common/src/main/java/services/WeaponSPI.java
@@ -0,0 +1,13 @@
+package services;
+
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+
+public interface WeaponSPI {
+    @Spawns("weapon")
+    public Entity createWeapon(SpawnData data);
+
+    public void loadWeapon(Entity weapon);
+
+}

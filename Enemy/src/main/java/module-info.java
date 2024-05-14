ca697d1afa005dfdf6e7ff4a17865fc876d14464
Joachim Hviid <47396357+joachimhviid@users.noreply.github.com>
2024-05-14 10:47:04 +0200

    Delete Enemy directory

--- Enemy/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module enemy {
-    requires com.almasb.fxgl.all;
-    requires common.enemy;
-    requires common;
-    requires common.ai;
-
-    exports enemysystem to com.almasb.fxgl.core;
-    provides common.enemy.EnemySPI with enemysystem.EnemyFactory;
-}

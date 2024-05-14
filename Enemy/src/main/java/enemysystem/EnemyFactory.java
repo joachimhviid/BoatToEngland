561dfadf98214c820af58c6fcb0f18933b33d209
joachimhviid <joac1099@gmail.com>
2024-05-14 10:45:02 +0200

    Cleanup: Removed empty classes and enforced naming convention

--- Enemy/src/main/java/enemysystem/EnemyFactory.java
+++ Enemy/src/main/java/enemysystem/EnemyFactory.java
@@ -4,13 +4,10 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.EntityFactory;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.Spawns;
-import com.almasb.fxgl.physics.PhysicsComponent;
 import common.ai.IPathFinder;
 import common.data.ServiceRegistry;
-import common.enemy.Enemy;
 import common.enemy.EnemySPI;
 import common.data.EntityType;
-import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
